fe3ea77b078da483974c52dee3067803fa6b604b
sebai-rayen <rayensebai183@gmail.com>
2026-04-06 16:05:07 +0100

    chore: ajouter l'application principale et le test

---
+++ bank-supervision-backend/src/main/java/tn/isam/spring/bankSupervision/BankSupervisionApplication.java
@@ -0,0 +1,14 @@
+package tn.isam.spring.bankSupervision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSupervisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSupervisionApplication.class, args);
+    }
+
+
+}
---
+++ bank-supervision-backend/src/test/java/tn/isam/spring/bankSupervision/Demo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.isam.spring.bankSupervision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
