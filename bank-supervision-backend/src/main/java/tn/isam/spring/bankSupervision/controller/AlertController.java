06f7b588c9cb685adfa86d69581a9424505b2eed
sebai-rayen <rayensebai183@gmail.com>
2026-04-26 23:53:39 +0100

    feat(alerts): Update AlertController for multi-recipient support

--- bank-supervision-backend/src/main/java/tn/isam/spring/bankSupervision/controller/AlertController.java
+++ bank-supervision-backend/src/main/java/tn/isam/spring/bankSupervision/controller/AlertController.java
@@ -1,16 +1,23 @@
 package tn.isam.spring.bankSupervision.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import tn.isam.spring.bankSupervision.dto.request.CreateAlertRequest;
 import tn.isam.spring.bankSupervision.dto.response.AdminAlertResponse;
+import tn.isam.spring.bankSupervision.dto.response.UserAlertResponse;
 import tn.isam.spring.bankSupervision.service.AlertService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/alerts")
-@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AlertController {
 
@@ -24,6 +31,17 @@ public class AlertController {
     @GetMapping("/latest")
     public AdminAlertResponse.IncomingAlert latest() {
         return alertService.getLatestAdminAlert().orElse(null);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserAlertResponse createAlert(@Valid @RequestBody CreateAlertRequest request) {
+        return alertService.createAlert(request);
+    }
+
+    @GetMapping("/my")
+    public List<UserAlertResponse> getMyAlerts() {
+        return alertService.getCurrentUserAlerts();
     }
 }
 
